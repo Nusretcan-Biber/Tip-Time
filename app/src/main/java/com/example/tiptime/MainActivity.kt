2cc486a052cc260b72d317dbefd3269cfbad7747
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2023-11-22 01:30:57 +0300

    son bikaç düzenleme

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,27 +18,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calgulateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
 
     }
         fun calgulateTip(){
-            val stringInTextField = binding.costOfServiceEditText.toString()
-            val cost = stringInTextField.toDouble()
-            val selectId = binding.tipOptions.checkedRadioButtonId
+            val stringInTextField = binding.costOfServiceEditText.text.toString()
+            val cost = stringInTextField.toDoubleOrNull()
 
-            val tipPercentage = when(selectId){
+            if (cost == null || cost == 0.0) {
+                displayTip(0.0)
+                return
+            }
+
+
+
+            val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
                 R.id.option_twenty_percent -> 0.20
-                R.id.options_fiftiin_percent -> 0.18
+                R.id.option_eighteen_percent -> 0.18
                 else -> 0.15
 
             }
             var tip = tipPercentage * cost
-            val roundUp = binding.roubndUpSwitch.isChecked
+            val roundUp = binding.roundUpSwitch.isChecked
             if (roundUp){
                 tip = kotlin.math.ceil(tip)
 
             }
+            displayTip(tip)
            // NumberFormat.getCurrencyInstance()
-            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-            binding.tipResult.text= getString(R.string.tip_amount,formattedTip)
         }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
